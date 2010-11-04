2e8d2732e0df302a676723a7952f1cbc90d8427d
Daniel Reverri <reverri@gmail.com>
2010-11-04 15:59:46 -0700

    Initial import from Confluence

---
+++ images/load_stocks.rb
@@ -0,0 +1,12 @@
+require 'csv'
+require 'riak'
+
+client = Riak::Client.new
+bucket = client.bucket('goog', :keys => false)
+
+CSV.foreach('goog.csv', :headers => true) do |row|
+  puts row.first[1].to_s
+  obj = bucket.new(row.first[1].to_s)
+  obj.data = Hash[row.to_a]
+  obj.store
+end
