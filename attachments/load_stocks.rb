868ab62c2ef14002d1c298d93457458711d470bf
Scott Gonyea <me@sgonyea.com>
2011-03-08 12:33:56 -0800

    Comment allows for a different port

--- attachments/load_stocks.rb
+++ attachments/load_stocks.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
 %w[rubygems csv time riak].each{|lib| require lib}
 
-client = Riak::Client.new
-bucket = client['goog']
+client  = Riak::Client.new # or: Riak::Client.new(:port => 8097)
+bucket  = client['goog']
 
-quotes = CSV.read 'goog.csv'
-header = quotes.shift
+quotes  = CSV.read 'goog.csv'
+header  = quotes.shift
 
 quotes.each do |row|
   obj = bucket[p row.first]
